69267d95d22e9ca2b2479e36d1c6126b1a052f66
Sahan Fernando <sahan@xelleron.com>
2024-06-25 09:57:33 +0530

    Add users controller.

---
+++ FoodDiaryApp/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDiaryApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Login");
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var loginUser = _context.Users
+                .FirstOrDefault(u => u.Username == user.Username && u.Password == user.Password);
+
+            if (loginUser != null)
+            {
+                // You might want to add authentication logic here
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(user);
+        }
+    }
+}
---
+++ FoodDiaryApp/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodDiaryApp.Models
+{
+    public class UserViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
